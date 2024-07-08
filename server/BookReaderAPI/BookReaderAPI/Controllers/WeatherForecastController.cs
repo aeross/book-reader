07fedb0896f7d2915ca6e5c25766b79eddf976ec
aeross <andrewpratama22@gmail.com>
2024-07-08 19:58:26 +0700

    established connection to postgres

--- server/BookReaderAPI/BookReaderAPI/Controllers/WeatherForecastController.cs
+++ server/BookReaderAPI/BookReaderAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace BookReaderAPI.Controllers
 {
@@ -12,15 +13,36 @@ namespace BookReaderAPI.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private IConfiguration Configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            bool boolfound = false;
+            using (NpgsqlConnection conn = new NpgsqlConnection(Configuration.GetConnectionString("Default")))
+            {
+                conn.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM books", conn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    boolfound = true;
+                    Console.WriteLine("connection established");
+                }
+                if (boolfound == false)
+                {
+                    Console.WriteLine("Data does not exist");
+                }
+                dr.Close();
+            }
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
